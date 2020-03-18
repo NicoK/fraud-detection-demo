0e4699e8546b01d3d8ba894c2407b70eca498999
Nico Kruber <nico@ververica.com>
2020-03-18 21:24:20 +0100

    [hotfix] modify a few log statements and don't use them for the normal workflow
    - there is no need to print all elements that are being processed (and if there
      is, people can add that)
    - log statements showing what rules have been processed/removed are demoted to
      TRACE now
    - added some more details on (de)serialization failures

--- flink-job/src/main/java/com/ververica/field/dynamicrules/functions/DynamicAlertFunction.java
+++ flink-job/src/main/java/com/ververica/field/dynamicrules/functions/DynamicAlertFunction.java
@@ -138,7 +138,7 @@ public class DynamicAlertFunction
   @Override
   public void processBroadcastElement(Rule rule, Context ctx, Collector<Alert> out)
       throws Exception {
-    log.info("{}", rule);
+    log.trace("Processing {}", rule);
     BroadcastState<Integer, Rule> broadcastState =
         ctx.getBroadcastState(Descriptors.rulesDescriptor);
     handleRuleBroadcast(rule, broadcastState);
@@ -168,7 +168,7 @@ public class DynamicAlertFunction
         while (entriesIterator.hasNext()) {
           Entry<Integer, Rule> ruleEntry = entriesIterator.next();
           rulesState.remove(ruleEntry.getKey());
-          log.info("Removed Rule {}", ruleEntry.getValue());
+          log.trace("Removed {}", ruleEntry.getValue());
         }
         break;
     }
